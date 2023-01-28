7ee045ea55863fc27c77cf78183f225d8645826d
jaj243 <123598035+jaj243@users.noreply.github.com>
2023-01-28 16:33:52 -0500

    Update Portfolio tracker BETA.py

--- Portfolio tracker BETA.py
+++ Portfolio tracker BETA.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 class PortfolioTracker:
     def __init__(self):
         self.portfolio = pd.DataFrame(columns=["Ticker", "Shares", "Price", "Dividend","Beta"])
+        self.rfr = 0.05 # risk-free rate
     
     def add_stock(self, ticker, shares):
         stock = yf.Ticker(ticker)
@@ -25,27 +26,24 @@ class PortfolioTracker:
         self.portfolio.loc[self.portfolio.Ticker == ticker, "Price"] = stock_info["regularMarketPrice"]
         self.portfolio.loc[self.portfolio.Ticker == ticker, "Dividend"] = stock_info["regularMarketDividend"]
         self.portfolio.loc[self.portfolio.Ticker == ticker, "Beta"] = stock_info["beta"]
+        
     def current_value(self):
         return self.portfolio["Shares"].mul(self.portfolio["Price"]).sum()
-
+    
     def total_dividend(self):
         return self.portfolio["Shares"].mul(self.portfolio["Dividend"]).sum()
-
+    
     def portfolio_beta(self):
         beta_weighted = self.portfolio["Shares"] * self.portfolio["Beta"]
         total_shares = self.portfolio["Shares"].sum()
         return beta_weighted.sum() / total_shares
-
-    def print_portfolio(self):
-        print(self.portfolio)
-        
+    
     def plot_portfolio(self):
         self.portfolio.plot(kind='bar', x='Ticker', y=['Shares', 'Price', 'Dividend'])
         plt.show()
-
-
-# In[ ]:
-
-
-
+        
+    def sharpe_ratio(self):
+        portfolio_return = (self.current_value() - self.initial_investment) / self.initial_investment
+        std_dev_return = self.portfolio["Price"].pct_change().std()
+        return (portfolio_return - self.rfr) / std_dev_return
 
